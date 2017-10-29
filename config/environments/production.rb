d28e8bca00b87c4bbe755372a4967da4db287bea
Astrico <astrico.aulia@gmail.com>
2017-10-30 00:50:56 +0700

    update plugin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,25 +1,21 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  # Code is not reloaded between requests.
-  config.cache_classes = true
+  # In the development environment your application's code is reloaded on
+  # every request.  This slows down response time but is perfect for development
+  # since you don't have to restart the webserver when you make code changes.
+  config.cache_classes = false
 
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
+  # Do not eager load code on boot.
+  config.eager_load = false
 
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local = false
-  config.action_controller.perform_caching = true
+  # Show full error reports and disable caching
+  config.consider_all_requests_local = true
+  config.action_controller.perform_caching = false
 
   # Disable delivery errors
   config.action_mailer.raise_delivery_errors = false
 
-  # No email in production log
-  config.action_mailer.logger = nil
-
-  # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
+  # Print deprecation notices to stderr and the Rails logger.
+  config.active_support.deprecation = [:stderr, :log]
 end
